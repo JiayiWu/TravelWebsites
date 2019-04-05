60328c914eed78712d4f10adba76ea4bb9008a58
yyy2015 <2456700983@qq.com>
2019-04-05 18:03:02 +0800

    不知道ignore为啥不生效啊

--- server/src/main/java/cn/edu/nju/travel/controller/StatisticController.java
+++ server/src/main/java/cn/edu/nju/travel/controller/StatisticController.java
@@ -1,8 +1,14 @@
 package cn.edu.nju.travel.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created on 2019/4/5
  */
+@RestController
+@RequestMapping("/statistic/")
 public class StatisticController {
+
 
 }
--- server/src/main/java/cn/edu/nju/travel/vo/MonthData.java
+++ server/src/main/java/cn/edu/nju/travel/vo/MonthData.java
@@ -1,8 +1,14 @@
 package cn.edu.nju.travel.vo;
 
+import lombok.Data;
+
 /**
  * Created on 2019/4/5
  */
+@Data
 public class MonthData {
 
+    private int userId;
+
+    private int[] data;
 }
