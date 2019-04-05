fc57507064f5d2b3d72f0e76fbc3dc7c81cd70d2
yyy2015 <2456700983@qq.com>
2019-04-05 17:55:16 +0800

    忽略配置

---
+++ server/src/main/java/cn/edu/nju/travel/controller/StatisticController.java
@@ -0,0 +1,8 @@
+package cn.edu.nju.travel.controller;
+
+/**
+ * Created on 2019/4/5
+ */
+public class StatisticController {
+
+}
---
+++ server/src/main/java/cn/edu/nju/travel/vo/MonthData.java
@@ -0,0 +1,8 @@
+package cn.edu.nju.travel.vo;
+
+/**
+ * Created on 2019/4/5
+ */
+public class MonthData {
+
+}
