79044995e94523746f9145fda770eb9f251d189f
yuhqqq <542772747@qq.com>
2019-04-05 23:58:43 +0800

    简陋的统计。。

--- client/src/containers/profile/ProfileStatis.tsx
+++ client/src/containers/profile/ProfileStatis.tsx
@@ -3,6 +3,7 @@ import { Form, Input, Button, message } from 'antd'
 import { FormComponentProps } from 'antd/lib/form/Form'
 import API from '../../utils/API'
 import messageHandler from '../../utils/messageHandler'
+import {fromJS} from "immutable";
 
 interface StatisProps {
 
@@ -13,129 +14,138 @@ class ProfileStatis extends React.Component<StatisProps &FormComponentProps, any
 
   componentDidMount() {
     var echarts = require('echarts')
-
+    var userId = localStorage.getItem('userid')
     // 用户参与活动数量与月份折线图
     var myChart = echarts.init(document.getElementById('activityNums'));
-
     var data = [19, 28, 0, 21, 7];
-    var xdata = ['12月', '1月', '2月', '3月', '4月'];
-    var option = {
-      title: { text: '用户参与活动数量' },
-      grid: {
-        left: 30,
-        right: 50,
-        top: 100,
-        bottom: 30,
-        containLabel: true
-      },
-      xAxis: {
-        type: 'category',
-        // boundaryGap: false,
-        data: xdata,
-        triggerEvent: true,
-        splitLine: {
-          show: false
+
+    API.query(`/statistic/monthData/${userId}`, {}).then((json) => {
+      if (json.code === 0) {
+        data = json.data;
+      }else {
+        data = [0,0,0,0,0];
+      }
+      var xdata = ['1月', '2月', '3月', '4月','5月'];
+      var option = {
+        title: { text: '我参与活动的数量' },
+        grid: {
+          left: 30,
+          right: 50,
+          top: 100,
+          bottom: 30,
+          containLabel: true
         },
-        axisLine: {
-          show: true,
-          lineStyle: {
-            width: 2,
-            color: 'rgba(255,255,255,.6)'
+        xAxis: {
+          type: 'category',
+          // boundaryGap: false,
+          data: xdata,
+          triggerEvent: true,
+          splitLine: {
+            show: false
+          },
+          axisLine: {
+            show: true,
+            lineStyle: {
+              width: 2,
+              color: 'rgba(255,255,255,.6)'
+            }
+          },
+          axisTick: {
+            show: false
+          },
+          axisLabel: {
+            color: '#000',
+            fontSize: 16,
           }
         },
-        axisTick: {
-          show: false
-        },
-        axisLabel: {
-          color: '#000',
-          fontSize: 16,
-        }
-      },
-      yAxis: {
-        name: '活动个数',
-        nameTextStyle: {
-          color: '#000',
-          fontSize: 16,
-          textShadowOffsetY: 2
-        },
-        type: 'value',
-        splitLine: {
-          show: true,
-          lineStyle: {
-            color: 'rgba(255,255,255,.2)'
+        yAxis: {
+          name: '活动个数',
+          nameTextStyle: {
+            color: '#000',
+            fontSize: 16,
+            textShadowOffsetY: 2
+          },
+          type: 'value',
+          splitLine: {
+            show: true,
+            lineStyle: {
+              color: 'rgba(255,255,255,.2)'
+            }
+          },
+          axisLine: {
+            show: true,
+            lineStyle: {
+              width: 2,
+              color: 'rgba(255,255,255,.6)'
+            }
+          },
+          axisTick: {
+            show: true
+          },
+          axisLabel: {
+            color: '#000',
+            fontSize: 16,
           }
         },
-        axisLine: {
-          show: true,
-          lineStyle: {
-            width: 2,
-            color: 'rgba(255,255,255,.6)'
-          }
-        },
-        axisTick: {
-          show: true
-        },
-        axisLabel: {
-          color: '#000',
-          fontSize: 16,
-        }
-      },
-      series: [
-        {
-        data: data,
-        type: 'line',
-        symbol: 'circle',
-        symbolSize: 12,
-        color: '#FEC201',
-        lineStyle: {
-          color: "#03E0F2"
-        },
-        label: {
-          show: true,
-          position: 'top',
-          textStyle: {
+        series: [
+          {
+            data: data,
+            type: 'line',
+            symbol: 'circle',
+            symbolSize: 12,
             color: '#FEC201',
-            fontSize: 18,
-            fontWeight: 'bold'
-          }
-        },
-        areaStyle: {
-          color: 'rgba(1,98,133,0.6)'
-        }
-      }, {
-        type: 'bar',
-        animation: false,
-        barWidth: 3,
-        hoverAnimation: false,
-        data: data,
-        tooltip: {
-          show: false
-        },
-        itemStyle: {
-          normal: {
-            color: {
-              type: 'linear',
-              x: 0,
-              y: 0,
-              x2: 0,
-              y2: 1,
-              colorStops: [{
-                offset: 0,
-                color: '#91EAF2' // 0% 处的颜色
-              }, {
-                offset: 1,
-                color: '#074863' // 100% 处的颜色
-              }],
-              globalCoord: false // 缺省为 false
+            lineStyle: {
+              color: "#03E0F2"
             },
             label: {
-              show: false
+              show: true,
+              position: 'top',
+              textStyle: {
+                color: '#FEC201',
+                fontSize: 18,
+                fontWeight: 'bold'
+              }
+            },
+            areaStyle: {
+              color: 'rgba(1,98,133,0.6)'
             }
-          }
-        }
-      }]
-    }
-    myChart.setOption(option)
+          }, {
+            type: 'bar',
+            animation: false,
+            barWidth: 3,
+            hoverAnimation: false,
+            data: data,
+            tooltip: {
+              show: false
+            },
+            itemStyle: {
+              normal: {
+                color: {
+                  type: 'linear',
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
+                  colorStops: [{
+                    offset: 0,
+                    color: '#91EAF2' // 0% 处的颜色
+                  }, {
+                    offset: 1,
+                    color: '#074863' // 100% 处的颜色
+                  }],
+                  globalCoord: false // 缺省为 false
+                },
+                label: {
+                  show: false
+                }
+              }
+            }
+          }]
+      }
+      myChart.setOption(option)
+    })
+
+
 
   }
   render() {
--- server/src/main/java/cn/edu/nju/travel/controller/StatisticController.java
+++ server/src/main/java/cn/edu/nju/travel/controller/StatisticController.java
@@ -4,10 +4,8 @@ import cn.edu.nju.travel.form.SimpleResponse;
 import cn.edu.nju.travel.service.StatisticService;
 import io.swagger.annotations.ApiOperation;
 import javax.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created on 2019/4/5
@@ -21,12 +19,15 @@ public class StatisticController {
 
     @ApiOperation(value = "获取用户每月参加的活动数量", response = SimpleResponse.class, notes =
             "返回大小为12的数组，值即为index对应的月份的次数（从0开始）")
-    @GetMapping("monthData")
-    public SimpleResponse getMonthData(@RequestParam int userId){
+    @GetMapping("monthData/{userid}")
+    public SimpleResponse getMonthData(@PathVariable("userid") int userId){
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         try{
             int[] data = statisticService.getMonthData(userId);
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             return SimpleResponse.ok(data);
         }catch (Exception e){
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
             return SimpleResponse.exception(e);
         }
     }
